cea51c63d5f4f6e92804cbcbd2d0001ddd2e342c
Tykan <Tykan@DESKTOP-K6E7G0U>
2026-08-27 18:26:42 +0300

    Adding exception interceptor for stable work

---
+++ src/Api/ExceptionInterceptor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api
+{
+    public class ExceptionInterceptor : ExceptionFilterAttribute, IAsyncExceptionFilter
+    {
+        public ExceptionInterceptor() {}
+
+        public override Task OnExceptionAsync(ExceptionContext context) => Task.CompletedTask;
+    }
+}
