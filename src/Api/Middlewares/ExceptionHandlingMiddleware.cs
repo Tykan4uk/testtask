36fbe7c535c95cc359b5561d3ad7a090dd4c5f36
Tykan <Tykan@DESKTOP-K6E7G0U>
2026-08-28 10:44:57 +0300

    Add middleware for handling exception

---
+++ src/Api/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,30 @@
+﻿namespace Api.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception)
+            {
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                context.Response.ContentType = "application/json";
+
+                await context.Response.WriteAsJsonAsync(new
+                {
+                    error = "Internal server error"
+                });
+            }
+        }
+    }
+}
